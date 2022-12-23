55fb8f5727126d012cf316ee8f98a8e70f0ccad6
renanguillen <rigcpj@gmail.com>
2022-12-23 10:53:53 -0300

    Automatic commit via Makefile

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 11:11:48 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/12/20 11:18:14 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/12/23 10:25:42 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 # define MINISHELL_H
 
 # include "../libft/includes/libft.h"
+
+typedef struct s_tokens
+{
+	char			*value;
+	int				type;
+	int				index;
+	struct s_tokens	*next;
+}				t_tokens;
+
+typedef struct s_shell
+{
+	int			state;
+	t_tokens	*tokens;
+}				t_shell;
 
 #endif
--- sources/ft_minishell.c
+++ sources/ft_minishell.c
@@ -6,13 +6,35 @@
 /*   By: ridalgo- <ridalgo-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 11:14:30 by ridalgo-          #+#    #+#             */
-/*   Updated: 2022/12/21 00:21:17 by ridalgo-         ###   ########.fr       */
+/*   Updated: 2022/12/23 10:29:27 by ridalgo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int main(int ac, char **av)
+static void	arguments(int ac)
 {
-	
+	if (ac > 1)
+	{
+		ft_printf("Please run ./minishell");
+		exit (1);
+	}
+}
+
+int	main(int ac, char **av)
+{
+	t_shell	*shell;
+
+	(void)av;
+	arguments(ac);
+	shell = ft_calloc(1, sizeof(t_shell));
+	shell->state = 0;
+	while (1)
+	{
+		if (shell->state == 1)
+			ft_printf("Shellstate == %d\n", shell->state);
+		else
+			ft_printf("Olá\n");
+	}
+	return (0);
 }
